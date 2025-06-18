1674e23e4ab01cd6fb17e99f9dc40cef69cf6234
okayoshi <a8616003@iwate-ua.c.jp>
2025-06-18 23:33:35 +0900

    修正

--- js/samples.js
+++ js/samples.js
@@ -3,7 +3,7 @@ const SAMPLE_SONGS = [
        { title: 'きらきら星', sequence: 'ド（き）ド（ら）ソ（き）ソ（ら）ラHi（ひ）ラHi（か）ソ（る）\nファ（お）ファ（そ）ミ（ら）ミ（の）レ（ほ）レ（し）ド（よ）' },
        { title: 'チューリップ', sequence: 'ド（さ）レ（い）ミ（た）ド（さ）レ（い）ミ（た）\nソ（ちゅー）ミ（りっ）レ（ぷ）ド（の）レ（は）ミ（な）レ（が）\nド（なー）レ（らん）ミ（だー）ド（なー）レ（らん）ミ（だー）\nソ（あ）ミ（か）レ（し）ド（ろ）レ（き）ミ（い）ド（ろ）\nソ（ど）ソ（の）ミ（は）ソ（な）ラHi（み）ラHi（て）ソ（も）\nミ（き）ミ（れ）レ（い）レ（だ）ド（な）' },
        { title: 'メリーさんのひつじ', sequence: 'ミレドレミミミ\nレレレミソソ\nミレドレミミミ\nミレレミレド' },
-       { title: 'the enternainer', sequence: 'ミファソドHiソドHiソドHi\nドHiレHiレ♯HiミHiドHiレHiミHiシHiレHiドHi\nミファソドHiソドHiソドHi\nラHiソファ♯ラHiドHiミHiレHiドHiラHiレHi\nミファソドHiソドHiソドHi\nドHiレHiレ♯HiミHiドHiレHiミHiシHiレHiドHi\nドHiレHiミHiドHiレHiミHiドHiレHiドHiミHiドHiレHiミHi\nドHiレHiドHiミHiドHiレHiミHiシHiレHiドHi' }
+       { title: 'The Enternainer', sequence: 'ミファソドHiソドHiソドHi\nドHiレHiレ♯HiミHiドHiレHiミHiシHiレHiドHi\nミファソドHiソドHiソドHi\nラHiソファ♯ラHiドHiミHiレHiドHiラHiレHi\nミファソドHiソドHiソドHi\nドHiレHiレ♯HiミHiドHiレHiミHiシHiレHiドHi\nドHiレHiミHiドHiレHiミHiドHiレHiドHiミHiドHiレHiミHi\nドHiレHiドHiミHiドHiレHiミHiシHiレHiドHi' }
     ];
     
     /* ========= サンプル曲リストの生成 ========= */
